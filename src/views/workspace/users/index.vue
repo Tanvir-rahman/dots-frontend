9c37eb9bb77e99f6975b75cd175e3cc1d22e2858
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-05-21 10:31:19 +0300

    feat(workspace-settings): workspace settings - peoples tab
    Added workspace people component to workspace settings,
    
    see #8

--- src/views/workspace/users/index.vue
+++ src/views/workspace/users/index.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row type="flex" justify="center" class="spacer">
       <el-col :span="12">
-        <h2>Workspace Users</h2>
+        <h2>Workspace Usessrs</h2>
       </el-col>
       <el-col :span="6">
         <h-search placeholder="Search user name or email" clearable />
@@ -14,8 +14,22 @@
         <h-button type="primary">{{ $t('actionVerbs.save') }}</h-button>
       </el-col>
     </el-row>
+    <el-row class="spacer">
+      <el-col :span="24">
+        <user-table />
+      </el-col>
+    </el-row>
   </div>
 </template>
+<script>
+import UserTable from './user-table'
+export default {
+  name: 'WorkspaceUsers',
+  components: {
+    UserTable
+  }
+}
+</script>
 <style scoped>
 .text-center {
   text-align: center;
--- src/views/workspace/users/user-table.vue
+++ src/views/workspace/users/user-table.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>user table</div>
+  <h-table :table-data="tableData" />
 </template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          name: 'Tom',
+          email: 'California',
+          lastlogin: '2020-07-02',
+          Role: 'Member',
+          Status: 'Active'
+        },
+        {
+          name: 'John',
+          email: 'Andalucia',
+          lastlogin: '2020-07-02',
+          Role: 'Member',
+          Status: 'Inactive'
+        },
+        {
+          name: 'John',
+          email: 'Andalucia',
+          lastlogin: '2020-07-02',
+          Role: 'Member',
+          Status: 'Active'
+        }
+      ]
+    }
+  }
+}
+</script>
